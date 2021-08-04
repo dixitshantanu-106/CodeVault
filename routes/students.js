0bec592bb1830718e61c3d66b8f3692f3afb5fb5
sam5299 <samshinde5299@gmail.com>
2021-08-04 10:45:51 +0530

    Resolving merge conflict

--- routes/students.js
+++ routes/students.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const {getStud, delStud, addStud, getAllStud, validate, studExists, addClass, addTeach, teachExists, classExits} = require('../services/students');
+const auth = require('../middleware/auth');
 
 // Fetch all the students related to a particular teacher
-router.get('/:tid', async (req, res) => {
-    const student = await getAllStud(req.params.tid);
+router.get('/', auth ,async (req, res) => {
+    const student = await getAllStud(req.userEmail._id);
     if (!student) return res.status(404).send('Student with the specified info not available');
     res.status(200).send(student);
 });
 
 // Fetch a particular student related to a particular teacher
-router.get('/:sid/:tid', async (req, res) => {
-    const student = await getStud(req.params.sid, req.params.tid);
+router.get('/:sid', auth , async (req, res) => {
+    const student = await getStud(req.params.sid, req.userEmail._id);
     if (!student) return res.status(404).send('Student with the specified info not available');
     res.status(200).send(student);
 });
